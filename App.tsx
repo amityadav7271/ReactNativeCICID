9d59c12530e95188013948498f89504be48141de
Amit Kumar Yadav <amityadav72719787@@gmail.com>
2025-12-24 23:58:47 +0530

    changes in ci yml

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
      <SafeAreaView style={{ flex: 1,backgroundColor: 'white' }}>
        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
-        <Text>Hello, World!</Text>
+        <Text>Hello, World! demo</Text>
          <Text>Environment: {Config.APP_ENV}</Text>
       </View>
      </SafeAreaView>

8d7196d7b4ee7d50af220bf9ffe2597ed04e0d3d
Amit Kumar Yadav <amityadav72719787@@gmail.com>
2025-12-24 22:59:21 +0530

    fix: CI env mapping for develop branch

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { Text, View } from "react-native";
 import Config from 'react-native-config';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 const App = () => {
   return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
+     <SafeAreaView style={{ flex: 1 }}>
+       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
         <Text>Hello, World!</Text>
          <Text>Environment: {Config.ENV}</Text>
       </View>
+     </SafeAreaView>
   );
 };
 

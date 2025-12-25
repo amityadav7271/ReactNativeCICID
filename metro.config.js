20d77e161382c3f7fb16c4f857097abb95b43ba9
Amit Kumar Yadav <amityadav72719787@@gmail.com>
2025-12-25 09:45:14 +0530

    fifing :CI/CD development branch build

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,28 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+// Polyfill for toReversed if not available
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function() {
+    return [...this].reverse();
+  };
+}
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // Add any resolver configuration here
+  },
+  transformer: {
+    // Add any transformer configuration here
+  },
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Workaround for toReversed compatibility issue
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = mergeConfig(defaultConfig, config);

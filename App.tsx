acf21fe5a11a6ddbbb0add61af6d09b1e93f7d94
Amit Kumar Yadav <amityadav72719787@@gmail.com>
2025-12-25 11:16:04 +0530

    fifing :CI/CD development branch build

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
      <SafeAreaView style={{ flex: 1,backgroundColor: 'white' }}>
        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
-        <Text>Hello, World! demo</Text>
+        <Text>Hello, World!</Text>
          <Text>Environment: {Config.APP_ENV}</Text>
       </View>
      </SafeAreaView>
